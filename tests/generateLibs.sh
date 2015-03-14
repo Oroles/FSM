67d604d5a0625930e050284b8cbdaea3ed0dc7ad
Oroles <vultur_horatiu@yahoo.com>
2015-03-14 12:37:36 +0100

    Improve tests
    Remove make files to create the libs and now is a scrip which creates the libs and copy them in tests folder

---
+++ tests/generateLibs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd ..
+path=${PWD}
+
+process_libs()
+{
+	cd $1
+	echo "new process libs"
+	echo $1
+	destination=$path"/tests/${1##*FSM/}"
+	for fileName in *.cpp
+	do
+		name="${fileName%.*}"
+		g++ -std=c++11 -c $fileName
+		ar rvs "$name.a" "$name.o"
+	done
+
+	mv *.a $destination
+	cp *.h $destination
+	cp *.hpp $destination
+
+	rm *.o
+
+	echo "end process libs"
+}
+
+walk_tree()
+{
+	for file in $(ls -rt -d -1 $1/*)
+	do
+		if [ -d $file ]; then
+			folderName="${file##*/}"
+			if [[ "$folderName" != "tests" ]]
+			then
+				process_libs $file
+				walk_tree $file
+			fi
+		fi
+	done
+}
+
+walk_tree $path
+
