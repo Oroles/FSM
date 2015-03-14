f635f057ef1ba3687949847294bcabe41b646f2e
Oroles <vultur_horatiu@yahoo.com>
2015-03-14 12:52:28 +0100

    Fix test
    Fix the test for State add the library to compiler

--- tests/testState.cpp
+++ tests/testState.cpp
@@ -5,6 +5,17 @@
 int main( int argc, char* argv[] )
 {
 	State s("ana");
-	State s1();
+	State s1;
+
+	if ( s == s1 )
+	{
+		return -1;
+	}
+
+	if ( s.getName() != "ana" )
+	{
+		return -1;
+	}
+
 	return 0;
 }
