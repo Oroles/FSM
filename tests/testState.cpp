49dcdc57968852d17d2eb922d793331e21b23816
Oroles <vultur_horatiu@yahoo.com>
2015-03-16 12:30:45 +0100

    Write tests for state

--- tests/testState.cpp
+++ tests/testState.cpp
@@ -6,16 +6,33 @@ int main( int argc, char* argv[] )
 {
 	State s("ana");
 	State s1;
+	State s2( "maria" );
+	State s3( s );
 
-	if ( s == s1 )
-	{
-		return -1;
-	}
+	/* Test constructors & equal operator */
+	if ( s == s1 ) return -1;
+	if ( s == s2 ) return -1;
+	if ( s1 == s2 ) return -1;
+	if ( !( s == s3 ) ) return -1;
 
-	if ( s.getName() != "ana" )
-	{
-		return -1;
-	}
+	/* Test operator != */
+	if ( s != s3 ) return -1;
+	if ( !( s != s1 ) ) return -1;
+
+	/* Test assign operator */
+	s1 = s;
+	if ( !( s1 == s ) ) return -1;
+	if ( s1 == s2  ) return -1;
+	if ( !( s1 == s3 ) ) return -1;
+
+	/* Test set and get name */
+	if ( s1.getName() != "ana" ) return -1;
+	if ( s.getName() != "ana" ) return -1;
+	if ( s2.getName() != "maria" ) return -1;
+	if ( s3.getName() != "ana" ) return -1;
+	s1.setName( "testName" );
+	if ( s1.getName() != "testName" ) return -1;
+	if ( s.getName() != "ana" ) return -1;
 
 	return 0;
 }
