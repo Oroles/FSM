96feca977dc3b62f0c0cb5e13ac66c20449deeb1
Oroles <vultur_horatiu@yahoo.com>
2015-04-06 10:10:10 +0200

    Update module tests

--- tests/tests/testModule.cpp
+++ tests/tests/testModule.cpp
@@ -12,5 +12,38 @@ int main( int argc, char* argv[] )
 	m.setName( "Process" );
 	TEST_EQUAL( m.getName(), "Process" );
 
+	TEST_EQUAL( m.nextState( State( "a" ) ), State( "a" ) );
+	m.addState( State( "a" ) );
+	TEST_EQUAL( m.nextState( State( "a" ) ), State( "a" ) );
+	m.setCurrentState( State( "b" ) );
+	TEST_EQUAL( m.nextState( State( "b" ) ), State( "b" ) );
+	m.addTranzition( Tranzition(State( "b" ) , State( "a" ) ) );
+	TEST_EQUAL( m.nextState( State( "b" ) ), State( "a" ) );
+
+	m.step();
+	TEST_EQUAL( m.nextState( State( "a" ) ), State( "a" ) );
+	m.setCurrentState( State( "b" ) );
+	TEST_EQUAL( m.nextState( State( "b" ) ), State( "a" ) );
+
+	m.addTranzition( Tranzition( State( "b" ), State( "c" ) ) );
+	TEST_EQUAL( m.nextState( State( "b" ) ), State( "a" ) );
+	m.addTranzition( Tranzition( State( "a" ), State( "b" ) ) );
+	TEST_EQUAL( m.nextState( State( "a" ) ), State( "b" ) );
+
+	{
+		Module m1 = m;
+		TEST_EQUAL( m1.nextState( State( "b" ) ), State( "a" ) );
+		TEST_EQUAL( m1.nextState( State( "a" ) ), State( "b" ) );
+		TEST_EQUAL( m1.nextState( State( "c") ), State( "c" ) );
+	}
+
+	{
+		Module m1;
+		m1 = m;
+		TEST_EQUAL( m1.nextState( State( "b" ) ), State( "a" ) );
+		TEST_EQUAL( m1.nextState( State( "a" ) ), State( "b" ) );
+		TEST_EQUAL( m1.nextState( State( "c") ), State( "c" ) );
+	}
+
 	return 0;
 }
